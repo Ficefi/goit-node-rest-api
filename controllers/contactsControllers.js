79bb7c4481045720d7a16aeff9061603bf54029c
Ficefi <murdzamarkian54@gmail.com>
2024-04-03 21:15:02 +0300

    did the task

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,70 @@
-import contactsService from "../services/contactsServices.js";
+import {
+	listContacts,
+	getContactById,
+	removeContact,
+	addContact,
+	refreshContact,
+} from "../services/contactsServices.cjs";
+import HttpError from "../helpers/HttpError.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+	const result = await listContacts();
+	res.json({
+		status: "success",
+		code: 200,
+		data: result,
+	});
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+	const { id } = req.params;
+	const result = await getContactById(id);
+	if (result == null) {
+		throw HttpError(404, "Not Found");
+	} else {
+		res.json({
+			status: "success",
+			code: 200,
+			data: result,
+		});
+	}
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res) => {
+	const { id } = req.params;
+	const result = await removeContact(id);
+	if (result == null) {
+		throw HttpError(404, "Not Found");
+	}
+	res.json({
+		status: "success",
+		code: 200,
+		data: result,
+	});
+};
 
-export const createContact = (req, res) => {};
+export const createContact = async (req, res) => {
+	const { name, email, phone } = req.body;
+	const result = await addContact(name, email, phone);
+	console.log(name, email, phone);
+	res.json({
+		status: "success",
+		code: 201,
+		data: result,
+	});
+};
 
-export const updateContact = (req, res) => {};
+export const updateContact = async (req, res) => {
+	const { id } = req.params;
+	const { name, email, phone } = req.body;
+	console.log(req.body);
+	const result = await refreshContact(id, { name, email, phone });
+	if (result == null) {
+		throw HttpError(404, "Not Found");
+	}
+	res.json({
+		status: "success",
+		code: 200,
+		data: result,
+	});
+};
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,20 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-
-})
+	name: Joi.string().alphanum().min(3).max(30).required(),
+	email: Joi.string()
+		.email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+		.required(),
+	phone: Joi.string()
+		.pattern(new RegExp("^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-s./0-9]*$"))
+		.min(6)
+		.required(),
+});
 
 export const updateContactSchema = Joi.object({
-
+	name: Joi.string().alphanum().min(3).max(30),
+	email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } }),
+	phone: Joi.string().pattern(new RegExp("^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-s./0-9]*$")).min(6),
 })
+	.min(1)
+	.message("Body must have at least one field");
